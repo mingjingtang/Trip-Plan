058002defcd766394ff9759fa35b8f8f1292e841
Mingjing Tang <tmjcsh@gmail.com>
2019-06-04 13:18:27 -0400

    fixed table column conflix and seed all data

--- db/migrate/20190604040859_create_trips.rb
+++ db/migrate/20190604040859_create_trips.rb
@@ -2,7 +2,7 @@ class CreateTrips < ActiveRecord::Migration[5.2]
   def change
     create_table :trips do |t|
       t.string :name
-      t.string :type
+      t.string :category
       t.string :region
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_040859) do
+ActiveRecord::Schema.define(version: 2019_06_04_160922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "trips", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "category"
     t.string "region"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema.define(version: 2019_06_04_040859) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "trips", "users"
 end

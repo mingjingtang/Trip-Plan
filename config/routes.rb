6baa4219d278e7b9fd5f87ebd80cf4051ee8cc0f
Mingjing Tang <tmjcsh@gmail.com>
2019-06-04 14:54:06 -0400

    create first three routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/places", to: "places#index"
-  resource :restaurants
+  # get "/places", to: "places#index"
+  # resource :restaurant
+
+  root to: 'root#index'
+  resources :places
+  resources :restaurants
 end

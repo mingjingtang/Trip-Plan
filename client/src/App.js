5d6eecee25076fb2f26601c83007ba9badd1b5b4
Mingjing Tang <tmjcsh@gmail.com>
2019-06-07 01:37:39 -0400

    check logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import {Route, Link, Switch, Redirect} from 'react-router-dom';
+import { Route, Link, Switch, Redirect } from 'react-router-dom';
 import PlaceResult from './components/PlaceResult/PlaceResult'
 import TripResult from './components/TripResult/TripResult'
 import Home from './components/Home/Home'
 import Login from './components/Login/Login'
 import axios from 'axios'
 import './App.css';
-import { Menu, Segment,Card, Icon, Image } from 'semantic-ui-react'
+import { Menu, Segment, Card, Icon, Image } from 'semantic-ui-react'
 
 
 class App extends Component {
@@ -21,7 +21,7 @@ class App extends Component {
 
       activeItem: 'home',
       tripName: "",
-      tripCategory:"",
+      tripCategory: "",
       tripRegion: ""
     };
   }
@@ -38,6 +38,7 @@ class App extends Component {
     this.setState({ tripsData: trips.data, apiDataLoadedTrips: true });
   }
 
+
   editTrip = (name, category, region) => {
     console.log(name, category, region)
     this.setState({
@@ -47,49 +48,47 @@ class App extends Component {
     })
   }
 
+
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
-  handleDelete = async(event, id) => {
+
+  handleDelete = async (event, id) => {
     console.log(this.props.id)
     event.preventDefault()
 
     await axios.delete(
-    `http://localhost:4567/users/1/trips/${id}`
-
-
- )
-      this.rerenderStuff()
- 
-}
-
-handleUpdate = async(event, id, data) => {
-  console.log(this.props.id)
-  event.preventDefault()
-
-  await axios.put(
-  `http://localhost:4567/users/1/trips/${id}`, data)
-
-
-
-
+      `http://localhost:4567/users/1/trips/${id}`
+    )
     this.rerenderStuff()
 
-}
-rerenderStuff = async() => {
-  let getData = await axios.get(
-    `http://localhost:4567/users/1/trips/`       
+  }
 
- )
- this.setState({
-    tripsData: getData.data
- })
-}
 
-  handleItemClick1 = async(newPlace) => {
+
+  handleUpdate = async (event, id, data) => {
+    console.log(this.props.id)
+    event.preventDefault()
+
+    await axios.put(
+      `http://localhost:4567/users/1/trips/${id}`, data)
+    this.rerenderStuff()
+
+  }
+  rerenderStuff = async () => {
+    let getData = await axios.get(
+      `http://localhost:4567/users/1/trips/`
+
+    )
+    this.setState({
+      tripsData: getData.data
+    })
+  }
+
+  handleItemClick1 = async (newPlace) => {
     console.log('this component is clicked')
 
     await this.setState(prevState => ({
-      tripsData: [...prevState.tripsData,newPlace],
+      tripsData: [...prevState.tripsData, newPlace],
     }))
 
     console.log(this.state.tripsData)
@@ -100,97 +99,96 @@ rerenderStuff = async() => {
 
   handleItemClick2 = async (id) => {
     console.log('this is the id i am going to delete ' + id)
-    const {tripsData} = this.state
+    const { tripsData } = this.state
     console.log('trip before splice' + this.state.tripsData)
     tripsData.splice(id, 1);
     console.log('trip after splice ' + this.state.tripsData)
-    
+
     await this.setState(prevState => ({
-        favoriteBooks: this.state.tripsData.filter(b=>{
-            return b.isTrip === true
-        })
+      favoriteBooks: this.state.tripsData.filter(b => {
+        return b.isTrip === true
+      })
     }))
   }
 
 
   render() {
     const { activeItem } = this.state
-    // console.log(this.state)
     return (
       <div>
         <Menu pointing secondary>
           <Link to="/">
-          <Menu.Item 
-            name='Home' 
-            active={activeItem === 'Home'} 
-            onClick={this.handleItemClick} />
+            <Menu.Item
+              name='Home'
+              active={activeItem === 'Home'}
+              onClick={this.handleItemClick} />
           </Link>
-            
+
           <Link to="/PlaceResult">
-          <Menu.Item 
-              name='Places' 
+            <Menu.Item
+              name='Places'
               active={activeItem === 'Places'}
               onClick={this.handleItemClick}
-              />
+            />
           </Link>
-              
+
 
           <Link to="/TripResult">
-          <Menu.Item
-            name='Your Trips'
+            <Menu.Item
+              name='Your Trips'
               active={activeItem === 'Your Trips'}
               onClick={this.handleItemClick}
-          />
+            />
           </Link>
 
 
           <Menu.Menu position='right'>
-          <Link to="Login">
-            <Menu.Item
-              name='login'
-              active={activeItem === 'login'}
-              onClick={this.handleItemClick}
-            />
+            <Link to="Login">
+              <Menu.Item
+                name='login'
+                active={activeItem === 'login'}
+                onClick={this.handleItemClick}
+              />
             </Link>
           </Menu.Menu>
         </Menu>
 
 
         <main>
-          <Switch> 
-          <Route 
-            exact path = "/placeresult"
-            render={()=><PlaceResult
-              places = {this.state.placesData}
-              trips = {this.state.tripsData}
-              onClick1={this.handleItemClick1}
-              onClick2={this.handleItemClick2}
-            />}
-          />
+          <Switch>
+            <Route
+              exact path="/placeresult"
+              render={() => <PlaceResult
+                places={this.state.placesData}
+                trips={this.state.tripsData}
+                onClick1={this.handleItemClick1}
+                onClick2={this.handleItemClick2}
+              />}
+            />
 
-          <Route
-            path = "/tripresult"
-            render={()=><TripResult
-              trips = {this.state.tripsData}
-              onClick2={this.handleItemClick2}
-              onClick3={this.handleDelete}
-              render={this.rerenderStuff}
-              update={this.handleUpdate}
-              editTrip={this.editTrip}
-            />}
-          />
+            <Route
+              path="/tripresult"
+              render={() => <TripResult
+                trips={this.state.tripsData}
+                onClick2={this.handleItemClick2}
+                onClick3={this.handleDelete}
+                render={this.rerenderStuff}
+                update={this.handleUpdate}
+                editTrip={this.editTrip}
+              />}
+            />
 
-          <Route
-            exact path = "/login"
-            render={()=><Login
-            />}
-          />
+            <Route
+              exact path="/login"
+              render={() => <Login
+              />}
+            />
 
-          <Route
-            path = "/"
-            render={()=><Home
-            />}
-          />
+            <Route
+              path="/"
+              render={() => <Home
+              />}
+            />
           </Switch>
         </main>
       </div>
--- client/src/components/TripItem/TripItem.js
+++ client/src/components/TripItem/TripItem.js
@@ -3,41 +3,40 @@ import '../TripItem/TripItem.css';
 import { Button, Image, List } from 'semantic-ui-react'
 import axios from 'axios'
 
-
 class TripItem extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             name: '',
             category: '',
-            region: '' 
+            region: ''
         };
     }
 
     handleChange = (event) => {
-        this.setState({ [event.target.name]:event.target.value });
+        this.setState({ [event.target.name]: event.target.value });
     }
 
-    handleSubmit = async(event) => {
-        // this.setState({ [event.target.name]:event.target.value });
+    handleSubmit = async (event) => {
         event.preventDefault();
         event.target.edit && console.log('edit')
         event.target.submit && console.log('submit')
 
-        
-            await axios.post(
-              "http://localhost:4567/users/1/trips",
-              {
+
+        await axios.post(
+            "http://localhost:4567/users/1/trips",
+            {
                 name: this.state.name,
                 category: this.state.category,
                 region: this.state.region,
                 user_id: 1
-              }
-            )
-        
-          this.props.render();
+            }
+        )
+        this.props.render();
     }
-    handleChange = (event)  => {
+
+
+    handleChange = (event) => {
         console.log(event.target.value)
         this.setState({
             [event.target.name]: event.target.value
@@ -50,37 +49,32 @@ class TripItem extends Component {
             name: e.target.value
         })
     }
-   updateStuff = (e, name, category, region) => {
-       console.log(e.target.name)
-    // this.setState({
-    //     name: e.target.name,
-    //     category: e.target.category,
-    //     region: e.target.region
-    // })
+    updateStuff = (e, name, category, region) => {
+        console.log(e.target.name)
 
-    let data = {
-    "name": this.state.name,
-    "category": this.state.category,
-    "region": this.state.region
+
+        let data = {
+            "name": this.state.name,
+            "category": this.state.category,
+            "region": this.state.region
+        }
+
+        this.props.update(e, this.props.id, data)
+        this.props.editTrip(name, category, region)
+        this.props.render()
+
     }
 
-       this.props.update(e, this.props.id, data)
-       this.props.editTrip(name, category, region)
-       this.props.render()
-
-   }
-
-   componentDidMount = () => {
-       this.setState({
-           name: this.props.tripName,
-           category: this.props.tripCategory,
-           region: this.props.tripRegion
-       })
-   }
+    componentDidMount = () => {
+        this.setState({
+            name: this.props.tripName,
+            category: this.props.tripCategory,
+            region: this.props.tripRegion
+        })
+    }
 
 
     render() {
-        // console.log(this.state)
         return (
             <div className="tripItem">
                 <div>
@@ -91,35 +85,40 @@ class TripItem extends Component {
                     <br></br>
                 </div>
 
-            
+
 
                 <form onSubmit={this.handleSubmit}>
                     <label>
-                        Name:
-                    <input type="text" name="name" value={this.state.value} onChange={this.handleChange} />
-                    <input type="text" name="category" value={this.state.value} onChange={this.handleChange} />
-                    <input type="text" name="region" value={this.state.value} onChange={this.handleChange} />
+                        Name
                     </label>
-                    <button type="submit" id="submit" name="submit">submit</button>
+                    <input type="text" name="name" value={this.state.value} onChange={this.handleChange} />
+
+                    <label>
+                        category
+                    </label>
+                    <input type="text" name="category" value={this.state.value} onChange={this.handleChange} />
+                    
+                    <label>
+                        region
+                    </label>
+                    <input type="text" name="region" value={this.state.value} onChange={this.handleChange} />
+                  
+                    <button type="submit" id="submit" name="submit">Add</button>
+
+                    <button className="button is-rounded"
+                    onClick={(e) => this.props.onClick3(e, this.props.id)} >
+                    Delete
+                </button>
                 </form>
 
 
-                <button className="button is-rounded"
-                    onClick={this.props.update}>
-                    Edit
-                </button>
-
-                <button className="button is-rounded"
-                    onClick={(e)=> this.props.onClick3(e,this.props.id)} >
-                    Delete
-                </button>
-
+            
                 <form onSubmit={e => this.updateStuff(e, this.state.name, this.state.category, this.state.region)}>
                     <label>
                         Name:
                     <input type="text" name="name" value={this.state.name} onChange={this.updateName} />
-                    <input type="text" name="category" value={this.state.category} onChange={this.handleChange} />
-                    <input type="text" name="region" value={this.state.region} onChange={this.handleChange} />
+                        <input type="text" name="category" value={this.state.category} onChange={this.handleChange} />
+                        <input type="text" name="region" value={this.state.region} onChange={this.handleChange} />
                     </label>
                     <button type="submit" id="submit" name="Edit">Edit</button>
                 </form>
